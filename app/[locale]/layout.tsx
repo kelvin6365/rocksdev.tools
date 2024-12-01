5b7f45ccade1ae4fecb874c2a5ca47284425fa7e
kelvin_kwong <2rocksstudio@gmail.com>
2024-12-02 00:33:33 +0800

    ads

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,15 +1,16 @@
+import { NavBar } from "@/components/nav-bar";
 import { routing } from "@/i18n/routing";
 import { cn } from "@/lib/utils";
 import { ToastProvider } from "@/providers/toast-provider";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { Inter } from "next/font/google";
 import { notFound } from "next/navigation";
-import { NavBar } from "@/components/nav-bar";
-import { GoogleAnalytics } from "@next/third-parties/google";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import GoogleAdsense from "../../components/adsense";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -36,6 +37,7 @@ export default async function RootLayout({
   return (
     <html lang={locale} suppressHydrationWarning>
       <body className={cn(inter.className, "min-h-screen bg-background")}>
+        <GoogleAdsense pId={process.env.NEXT_PUBLIC_ADSENSE_ID ?? ""} />
         <NextIntlClientProvider locale={locale} messages={messages}>
           <NavBar />
           <main className="container py-6">{children}</main>
