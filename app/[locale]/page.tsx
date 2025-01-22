d279d3470cf9f928ba12aee2a54894e3d52aa9f0
kelvin_kwong <2rocksstudio@gmail.com>
2025-01-22 22:37:37 +0800

    ad styles

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -225,7 +225,9 @@ export default function HomePage() {
       </section>
       <div className="h-12"></div>
       <div className="flex justify-center">
-        <AdUnit adSlot="2962196569" />
+        <div className="w-[728px] h-[90px]">
+          <AdUnit adSlot="2962196569" />
+        </div>
       </div>
     </div>
   );
